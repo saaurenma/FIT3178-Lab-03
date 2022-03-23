635f3aab06b3573b8fc1a857d1b1b168e040b40c
Saauren Mankad <sman0039@student.monash.edu>
2022-03-23 17:20:13 +1100

    Completed Extension Part 3

--- lab03/CurrentPartyTableViewController.swift
+++ lab03/CurrentPartyTableViewController.swift
@@ -14,8 +14,7 @@ class CurrentPartyTableViewController: UITableViewController, AddSuperHeroDelega
     let SECTION_MARVEL = 0
     let SECTION_DC = 1
     
-    let SECTION_HERO = 0
-    let SECTION_INFO = 1
+    let SECTION_INFO = 2
     
     let CELL_HERO = "heroCell"
     let CELL_INFO = "partySizeCell"
@@ -43,22 +42,29 @@ class CurrentPartyTableViewController: UITableViewController, AddSuperHeroDelega
         
         let marvelHeroNames = currentParty[SECTION_MARVEL].filter({$0.name == newHero.name})
         let DCHeroNames = currentParty[SECTION_DC].filter({$0.name == newHero.name})
-
+        
+        
         tableView.performBatchUpdates({
             if marvelHeroNames == [] && DCHeroNames == []{
                 
                 if newHero.universe?.rawValue == SECTION_MARVEL {
                     currentParty[SECTION_MARVEL].append(newHero)
+                    tableView.insertRows(at: [IndexPath(row: (currentParty[SECTION_MARVEL].count) - 1, section:
+                SECTION_MARVEL)],
+                        with: .automatic)
                 }
                 
                 else if newHero.universe?.rawValue == SECTION_DC {
                     currentParty[SECTION_DC].append(newHero)
+                    tableView.insertRows(at: [IndexPath(row: (currentParty[SECTION_DC].count) - 1, section:
+                SECTION_DC)],
+                        with: .automatic)
                 }
                 
-            tableView.insertRows(at: [IndexPath(row: (currentParty[SECTION_MARVEL].count + currentParty[SECTION_DC].count) - 1, section:
-        SECTION_HERO)],
-                with: .automatic)
+                
+
                 tableView.reloadSections([SECTION_INFO], with: .automatic) }},completion: nil)
+        
         return true
     }
     
@@ -73,8 +79,12 @@ class CurrentPartyTableViewController: UITableViewController, AddSuperHeroDelega
         
         switch section {
             
-        case SECTION_HERO:
-            return currentParty[SECTION_MARVEL].count + currentParty[SECTION_DC].count
+        case SECTION_DC:
+            return currentParty[SECTION_DC].count
+            
+        case SECTION_MARVEL:
+            return currentParty[SECTION_MARVEL].count
+            
         
         case SECTION_INFO:
             return 1
@@ -87,22 +97,44 @@ class CurrentPartyTableViewController: UITableViewController, AddSuperHeroDelega
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == SECTION_HERO {
+        
+
+        
+        if indexPath.section == SECTION_MARVEL{
+              print("marvel")
+              let heroCell = tableView.dequeueReusableCell(withIdentifier: CELL_HERO, for: indexPath)
+              
+              var content = heroCell.defaultContentConfiguration()
             
+
+            
+            let hero = currentParty[SECTION_MARVEL][indexPath.row]
+              
+              content.text = hero.name
+              content.secondaryText = hero.abilities
+              heroCell.contentConfiguration = content
+              
+              return heroCell
+            
+            
+        }
+        
+        else if indexPath.section == SECTION_DC {
+            print("dc")
             let heroCell = tableView.dequeueReusableCell(withIdentifier: CELL_HERO, for: indexPath)
-                        
+            
             var content = heroCell.defaultContentConfiguration()
-            let marvelHero = currentParty[SECTION_MARVEL][indexPath.row]
-//            let DCHero = currentParty[SECTION_DC][indexPath.row]
+          
+
+          
+          let hero = currentParty[SECTION_DC][indexPath.row]
             
-            
-            
-            content.text = marvelHero.name
-            content.secondaryText = marvelHero.abilities
+            content.text = hero.name
+            content.secondaryText = hero.abilities
             heroCell.contentConfiguration = content
             
             return heroCell
-            
+          
             
         }
         
@@ -114,7 +146,7 @@ class CurrentPartyTableViewController: UITableViewController, AddSuperHeroDelega
             
             var content = infoCell.defaultContentConfiguration()
             
-            if currentParty.isEmpty{
+            if currentParty[SECTION_MARVEL].isEmpty && currentParty[SECTION_DC].isEmpty{
                 
                 content.text = "No heroes in Party. Tap + to add some."
             }
@@ -132,7 +164,7 @@ class CurrentPartyTableViewController: UITableViewController, AddSuperHeroDelega
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
     
-        if indexPath.section == SECTION_HERO {
+        if indexPath.section == SECTION_MARVEL || indexPath.section == SECTION_DC {
             return true
         }
         
@@ -143,24 +175,34 @@ class CurrentPartyTableViewController: UITableViewController, AddSuperHeroDelega
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
-        if editingStyle == .delete && indexPath.section == SECTION_HERO {
+        if editingStyle == .delete && indexPath.section == SECTION_DC {
             tableView.performBatchUpdates({
-                self.currentParty.remove(at: indexPath.row)
+                self.currentParty[SECTION_DC].remove(at: indexPath.row)
                 self.tableView.deleteRows(at: [indexPath], with: .fade)
                 self.tableView.reloadSections([SECTION_INFO], with: .automatic)
             }, completion:nil)
         }
         
+        else if editingStyle == .delete && indexPath.section == SECTION_MARVEL {
+            tableView.performBatchUpdates({
+                self.currentParty[SECTION_MARVEL].remove(at: indexPath.row)
+                self.tableView.deleteRows(at: [indexPath], with: .fade)
+                self.tableView.reloadSections([SECTION_INFO], with: .automatic)
+            }, completion:nil)
+        }
+        
+        
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection
                                 section: Int) -> String? {
+        
         if section == SECTION_MARVEL {
-            return "Marvel"
+            return "Marvel Heroes:"
         }
         
         else if section == SECTION_DC {
-            return "DC"
+            return "DC Heroes:"
         }
         
         return ""
